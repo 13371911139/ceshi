732fc2f985dcc816370c1ea076a9b85b7cfc37fc
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2017-09-21 10:34:52 +0800

    no message

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ if(process.env.NODE_ENV ==='dev'){
 
 app.use('/lexiugo-app', require('./api'));
 app.get('/server/:a',(req,res,next)=>{
-    console.log(req.query);
     var arr={loveCarRepair:'维修记录',lexiuApp:'修理厂',reportStatistics:'透明修车',newBuild:'案件推修'}
     var dataList={//'http://qq328532063.6655.la/dist/'+req.query.action ||
         path:'http://116.62.162.134:8090/server/dist/'+req.query.action || ripath+(req.query.action || 'lexiuApp'),
