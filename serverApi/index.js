e1f1a77b8cd40cee312ecd83ec4cdd2765d6ae0e
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2017-11-08 13:59:20 +0800

    no message

--- serverApi/index.js
+++ serverApi/index.js
@@ -73,8 +73,10 @@ router.post('/BQXX',(req,res,next)=>{
     var url="http://assess-api.lexiugo.com/assess-api/assess-api"+req.body.data
     superagent
         .get(url)
+        .type('form')
         .query({userName:"lexiugo",
             passwd:"n27H3lNGL7wJSePFsrr0g16UTU0%2BtDfsGHMVZ2pmxsDaFV4cVSzVwQ%3D%3D",})
+        .set('Accept','application/json')
         .end((reqe,rese)=>{
             console.log(rese,'asdfasdf',reqe);
             res.json(rese.body)
