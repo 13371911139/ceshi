1274d175706a557c4763a7b0c560fc973dd46c6a
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2017-11-28 19:43:33 +0800

    no message

--- serverApi/config/wxApi.js
+++ serverApi/config/wxApi.js
@@ -18,7 +18,7 @@ const wxApi={
             jsapi_ticket: tokenData.ticket,
             nonceStr: wxApi.createNonceStr(),
             timestamp: wxApi.createTimestamp(),
-            url: url
+            url: url.split('#')[0]
         }
         var string = wxApi.raw(ret);
         console.log(string)
