41179f677db34e080f461909e7f394e21c30c01c
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2017-11-28 18:41:16 +0800

    no message

--- serverApi/config/wxApi.js
+++ serverApi/config/wxApi.js
@@ -1,7 +1,9 @@
 var superagent = require('superagent');
+const APPID='wx4b3cc819d682ce0e';
+const APPSECRET='07826b26a4f149ccfacba09426fa980c'
 const wxApi={
     getToken:()=>{
-        superagent.get(global.getToken).accept('json').end(function(reqe,rese){
+        superagent.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+APPID+'&secret='+APPSECRET).accept('json').end(function(reqe,rese){
             console.log(rese,getHeader);
             res.json(rese.body)
         });
