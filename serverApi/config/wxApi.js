ee4c7b96fd020362f8a676627380a7a9f4ae0b1d
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2017-11-29 12:31:24 +0800

    no message

--- serverApi/config/wxApi.js
+++ serverApi/config/wxApi.js
@@ -60,7 +60,7 @@ const wxApi={
     getOpenId:(code,fun)=>{
         var getOps='https://api.weixin.qq.com/sns/oauth2/access_token?appid='+APPID+'&secret='+APPSECRET+'&code='+code+'&grant_type=authorization_code'
         superagent.get(getOps).accept('json').end(function(reqe,rese){
-            console.log(rese.body,'获取openid？？？？？',getOpenId)
+            console.log(rese.body,'获取openid？？？？？',getOps)
             fun && fun(rese.body)
         });
     }
