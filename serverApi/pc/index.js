ce1961d3464a4aa24c5920a696fd8bd2b0b5256b
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2017-11-30 19:36:23 +0800

    no message

--- serverApi/pc/index.js
+++ serverApi/pc/index.js
@@ -6,6 +6,6 @@ var router = express.Router();
 router.use('/',(req,res,next)=>{
     console.log(req.query,'////////',req.body)
     req.body={'taskId':'2c905cc760056b840160056e10990001','partId':'2c905cc760056b840160056e11210006'};
-    res.render('pc/peijianDs',{dataList:req.body});
+    res.render('./pc/peijianDs',{dataList:req.body});
 });
 module.exports = router;
