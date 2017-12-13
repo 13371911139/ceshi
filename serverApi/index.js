3d6df0b922acbbcec8a08c80c1def3e3153337f6
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2017-12-13 23:49:37 +0800

    no message

--- serverApi/index.js
+++ serverApi/index.js
@@ -154,7 +154,7 @@ router.post('/BQXX',(req,res,next)=>{
     var data={}
     data[arrs[keys].keys]=dats
     console.log(data,arrs[keys].url)
-    var urlse='http://localhost:8888/toumingxiu/brands'+arrs[keys].url+'.do'
+    var urlse='http://localhost:8888/toumingxiu/app/'+arrs[keys].url+'.do'
     superagent
         .post(urlse)
         .type('form')
