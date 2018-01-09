5a7d8f79db30d4bb9be1cfebf2161fd8ca2f0123
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2018-01-09 14:46:49 +0800

    no message

--- serverApi/index.js
+++ serverApi/index.js
@@ -473,10 +473,6 @@ router.post('/pcMapXlc',(req,res,next)=>{
    '<script>' +
     '$("#mapSelectBox").ready(function(){' +
     'var script = document.createElement(\'script\');script.src=\'http://116.62.162.134:8090/server/dist/pcSelectMap.js\';' +
-    '' +
-    'console.log($(script));' +
-    '$("body").append($(script))' +
-
     'document.getElementById(\'mapSelectBox\').appendChild(script);' +
     '})' +
 
