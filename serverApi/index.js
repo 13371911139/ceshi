505e9f63eed08bd0390944e71831ed5ffcd77156
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2018-01-09 12:32:29 +0800

    no message

--- serverApi/index.js
+++ serverApi/index.js
@@ -467,11 +467,9 @@ router.post('/pcMapXlc',(req,res,next)=>{
     '<section id="appWrappers"></section>' +
     '<input type="hidden" id="xlcRepairLevelNode" value="'+req.body.xlcRepairLevel+'">' +
     '<input type="hidden" id="brandCodeNode" value="'+req.body.brandCode+'">' +
-    '<script >' +
-    'var scriptHtml=$('+"<script></script>"+');scriptHtml.attr("src","http://116.62.162.134:8090/server/dist/pcSelectMap.js";$("body").append(scriptHtml);' +
-    '</script>' +
+    '<script id="jbmtcbd"></script>' +
+    '<script >$("#jbmtcbd").attr("src","http://116.62.162.134:8090/server/dist/pcSelectMap.js")</script>' +
     '</div>' +
     ''})
 })
-/*$("body").append(<script src="http://116.62.162.134:8090/server/dist/pcSelectMap.js" ></script>)*/
 module.exports = router;
