540aebf0195f20dba8dd1eccbdd104aac416aa98
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2018-01-12 10:30:19 +0800

    no message

--- serverApi/pc/index.js
+++ serverApi/pc/index.js
@@ -3,7 +3,7 @@ var superagent = require('superagent');
 var router = express.Router();
 
 
-router.use('/',(req,res,next)=>{
+router.use('/peijianDS',(req,res,next)=>{
     console.log(req.query,'////////',req.body)
     req.body={'taskId':'2c905cc760056b840160056e10990001','partId':'2c905cc760056b840160056e11210006'};
     res.render('./pc/peijianDS',{dataList:req.body});
