c695c655aa2787938744a810edde8e5cf65e09a8
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2018-01-12 10:27:06 +0800

    no message

--- serverApi/pc/index.js
+++ serverApi/pc/index.js
@@ -8,4 +8,8 @@ router.use('/',(req,res,next)=>{
     req.body={'taskId':'2c905cc760056b840160056e10990001','partId':'2c905cc760056b840160056e11210006'};
     res.render('./pc/peijianDS',{dataList:req.body});
 });
+
+router.use('/mapMak',(req,res,next)=>{
+    res.render('./pc/mapForOther/mapMak',{dataList:req.body});
+})
 module.exports = router;
