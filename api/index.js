5a231eeadbb7a0e889f2e733e593152f930253f5
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2018-03-09 18:39:08 +0800

    no message

--- api/index.js
+++ api/index.js
@@ -1,15 +1,23 @@
 var express= require('express');
 var superagent = require('superagent');
+
 var router = express.Router();
 //express body-parser swig iconv-lite bluebird request
+var projjj=true;
+
 router.post('/*',(req,res,next)=>{
-    const data=req.body;
-    var projjj=true;
     if(projjj){
-        var apiURL = 'http://www.toumingxiuche.cn'+req.originalUrl;
+        var apiURL = 'http://www.beidouchaxun.cn'+req.originalUrl;
     }else{
-        var apiURL = '192.168.0.117:8080'+req.originalUrl;
+        // var apiURL = 'http://121.43.165.81:8888'+req.originalUrl;
+        var apiURL = 'http://192.168.0.129:8080'+req.originalUrl;
     }
+
+   if(req.headers.cookie){
+        var ServerCookie = req.headers.cookie;
+    }
+    const data=req.body;
+   console.log(apiURL,data)
     superagent
         .post(apiURL)
         .type('form')
@@ -17,14 +25,72 @@ router.post('/*',(req,res,next)=>{
         .send(data)
         .set('X-API-Key', 'foobar')
         .set('Accept', 'application/json')
+        .set('Cookie', ServerCookie)
         .end(function(reqe,rese){
-            console.log(rese.body);
-            res.json(rese.body)
+            try{
+                console.log(rese,reqe)
+                if(rese.ok) {
+                    if (rese.headers && rese.headers['set-cookie']) {
+                        var Cookies = {};
+                        rese.headers['set-cookie'][0].split(';').forEach(l => {
+                            var parts = l.split('=');
+                            Cookies[parts[0].trim()] = (parts[1] || '').trim();
+                        });
+                        res.cookie('app_sid', Cookies.app_sid)
+                    }
+                    if(rese.body){
+                        res.json(rese.body)
+                    }
+                }else{
+                    res.json({msg:'请求异常',code:'911',text:rese.text})
+                }
+            }catch (e){
+                console.log(e)
+                res.json({msg:'您请求的地址可能不存在',code:'119',text:rese ? rese.text :'地址错误'})
+
+            }
+
         });
 });
-router.get('/*',(req,res,next)=>{
-    console.log(typeof(data));
-    r.post({url:'http://www.toumingxiuche.cn'+req.originalUrl,form:req.body}).pipe(res);
+router.get('/',(req,res,next)=>{
+    if(projjj){
+        var apiURL = 'http://www.beidouchaxun.cn'+req.originalUrl;
+    }else{
+        var apiURL = '192.168.0.113:8080'+req.originalUrl;
+    }
 
+    if(req.headers.cookie){
+        var ServerCookie = req.headers.cookie;
+    }
+    const data=req.body;
+    superagent
+        .get(apiURL)
+        .accept('json')
+        .send(data)
+        .set('X-API-Key', 'foobar')
+        .set('Accept', 'application/json')
+        .end(function(reqe,rese){
+            try {
+                if (rese.ok) {
+                    if (rese.headers && rese.headers['set-cookie']) {
+                        var Cookies = {};
+                        rese.headers['set-cookie'][0].split(';').forEach(l => {
+                            var parts = l.split('=');
+                            Cookies[parts[0].trim()] = (parts[1] || '').trim();
+                        });
+                        res.cookie('app_sid', Cookies.app_sid)
+                    }
+                    if (rese.body) {
+                        res.json(rese.body)
+                    }
+                } else {
+                    res.json({msg: '请求异常', code: '911', text: rese.text})
+                }
+            }catch (e){
+                console.log(e)
+                res.json({msg:'您请求的地址可能不存在',code:'119',text:rese.text})
+
+            }
+        });
 });
 module.exports = router;
